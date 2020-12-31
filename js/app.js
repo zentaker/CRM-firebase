25f094cfd60e28bee2955d2a652c281b99e783d6
zentaker <46875264+zentaker@users.noreply.github.com>
2020-12-30 19:55:38 -0500

    actualizacion

--- js/app.js
+++ js/app.js
@@ -1,3 +1,34 @@
+
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      // User is signed in.
+        document.getElementById("user_div").style.display = "block";
+        document.getElementById("login_div").style.display = "none";
+    } else {
+      // No user is signed in.
+      document.getElementById("user_div").style.display = "none";
+      document.getElementById("login_div").style.display = "block";
+    }
+  });
+
 function login() {
+
+    
+    var userEmail = document.getElementById('email');
+    var userPassword = document.getElementById('password');
+
+    console.log(userEmail);
+
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPassword)
+
+  .catch((error) => {
+    var errorCode = error.code;
+      var errorMessage = error.message;
+      window.alert("Error: " + errorMessage);
+  });
+
+
+
+
     
 }
