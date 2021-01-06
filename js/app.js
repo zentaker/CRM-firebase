de3ddeeafd076ec2bc191173b5188e1c6b25cf6e
zentaker <46875264+zentaker@users.noreply.github.com>
2021-01-06 08:57:42 -0500

    login conectado a firebase

--- js/app.js
+++ js/app.js
@@ -1,6 +1,25 @@
 
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCxPbWhnISHclW_InK50hPbA2-EtM4PI2c",
+  authDomain: "crm-prueva.firebaseapp.com",
+  projectId: "crm-prueva",
+  storageBucket: "crm-prueva.appspot.com",
+  messagingSenderId: "830432938783",
+  appId: "1:830432938783:web:4e34904063ba3bce5cbe2a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+
+
+
+
+
 firebase.auth().onAuthStateChanged(function (user) {
-    if (user) {
+  if (user) {
+    console.log(user);
       // User is signed in.
         document.getElementById("user_div").style.display = "block";
         document.getElementById("login_div").style.display = "none";
@@ -14,8 +33,8 @@ firebase.auth().onAuthStateChanged(function (user) {
 function login() {
 
     
-    var userEmail = document.getElementById('email');
-    var userPassword = document.getElementById('password');
+    var userEmail = document.getElementById('email').value;
+    var userPassword = document.getElementById('password').value;
 
     console.log(userEmail);
 
@@ -32,3 +51,8 @@ function login() {
 
     
 }
+
+function logout() {
+  firebase.auth().signOut();
+}
+
